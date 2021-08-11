d745a00a8956169156fb132a44b9c09525f85407
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-11 11:14:13 -0400

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides_arr = [@a, @b, @c]
+  end
+
+  def kind 
+    if @sides_arr.all? { |side| side > 0 } && @a + @b > @c && @a + @c > @b && @b + @c > @a
+      if @sides_arr.uniq.length == 1
+        :equilateral
+      elsif @sides_arr.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+   
+  end
 end
